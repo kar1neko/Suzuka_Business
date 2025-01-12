d3ce3ee2d5e97a6e91c020f3dd75099bba354bd1
kar1neko <uguisutokarineko2007@gmail.com>
2025-01-12 18:38:42 +0900

    add project files

---
+++ app/src/main/java/com/example/suzuka_business_contest/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.suzuka_business_contest.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
