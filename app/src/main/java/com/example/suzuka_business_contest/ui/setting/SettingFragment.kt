e5e382e8a76dacefd2ef60ee2d77ef4ba32a2dd1
kar1neko <uguisutokarineko2007@gmail.com>
2025-01-13 21:28:54 +0900

    create fragment setting

---
+++ app/src/main/java/com/example/suzuka_business_contest/ui/setting/SettingFragment.kt
@@ -0,0 +1,27 @@
+package com.example.suzuka_business_contest.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.suzuka_business_contest.R
+import java.util.zip.Inflater
+
+class SettingFragment {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, constainer: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_setting, constainer, false)
+
+        //setting button's click-listener
+        val settingsButton: Button = view.findViewById(R.id.action_settings)
+        settingsButton.setOnClickListener {
+
+        }
+
+        return view
+    }
+}
