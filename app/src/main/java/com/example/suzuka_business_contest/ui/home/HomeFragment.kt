14a742e0f1cf6ec4661dd2f72796ca9189c790ed
kar1neko <uguisutokarineko2007@gmail.com>
2025-01-16 21:02:25 +0900

    update files

--- app/src/main/java/com/example/suzuka_business_contest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/suzuka_business_contest/ui/home/HomeFragment.kt
@@ -4,34 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.suzuka_business_contest.R
 import com.example.suzuka_business_contest.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.tvFirstSentence
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.BHomeUpgrade.setOnClickListener {
+            findNavController().navigate(R.id.action_homefragment_to_premiumfragment)
         }
+
         return root
     }
 
